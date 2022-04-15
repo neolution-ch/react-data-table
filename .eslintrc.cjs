29b789f658f2be1671f105e55d64b807ce98c37e
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2022-04-15 07:23:33 +0200

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ["eslint:recommended", "plugin:import/recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:import/recommended", "prettier", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
@@ -35,7 +35,6 @@ module.exports = {
     // https://github.com/typescript-eslint/typescript-eslint/issues/2621
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
-
     // Doesn't work with our neolution-ch packages
     "import/named": "off",
     "import/no-unresolved": "off",
@@ -44,15 +43,28 @@ module.exports = {
     "import/namespace": "off",
     "import/no-named-as-default": "off",
     "import/no-named-as-default-member": "off",
-
     // Enforce double quotes
-    quotes: ["error", "double", { avoidEscape: true }],
-
+    quotes: [
+      "error",
+      "double",
+      {
+        avoidEscape: true,
+      },
+    ],
     // Prefer string interpolation
     "prefer-template": "error",
-
-    "max-lines": ["error", { max: 200 }],
-    complexity: ["error", { max: 12 }],
+    "max-lines": [
+      "error",
+      {
+        max: 200,
+      },
+    ],
+    complexity: [
+      "error",
+      {
+        max: 12,
+      },
+    ],
     "prefer-destructuring": "error",
     "no-empty-function": "error",
     "arrow-body-style": ["error", "as-needed"],
