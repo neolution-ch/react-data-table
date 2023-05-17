c1724fd11f1c719abc87fbed2be88803a6e293f4
Andrea Aliberti <andrea.aliberti@neolution.ch>
2023-05-17 09:12:23 +0200

    Highlight comment fix

--- src/lib/DataTableHeader/DataTableRow.tsx
+++ src/lib/DataTableHeader/DataTableRow.tsx
@@ -70,7 +70,7 @@ export function DataTableRow<T, TRouteNames>({
     }
 
     if (operator_table[rowHighlight.operation](selectedValue, rowHighlight.compareValue)) {
-      return rowHighlight.customStyle ? rowHighlight.customStyle : defaultStyle;
+      return rowHighlight.customStyle ?? defaultStyle;
     }
 
     return undefined;
