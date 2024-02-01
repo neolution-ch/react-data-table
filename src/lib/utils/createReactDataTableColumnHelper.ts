9637c3f5e1fcb9e4ae0e7a47ba455232908f3375
neotob <tobias.merki@neolution.ch>
2024-02-01 14:58:34 +0100

    accept list additionally to record

--- src/lib/utils/createReactDataTableColumnHelper.ts
+++ src/lib/utils/createReactDataTableColumnHelper.ts
@@ -1,9 +1,10 @@
 ﻿import { ColumnDef, ColumnHelper, DeepKeys, RowData, createColumnHelper } from "@tanstack/react-table";
+import { DropdownColumnFilterOption } from "src/react-table";
 
 interface ReactDataTableColumnHelper<TData extends RowData> extends ColumnHelper<TData> {
   createEnumColumn: <TEnum extends string | number>(
     columnKey: DeepKeys<TData>,
-    enumTranslations: Record<TEnum, string>,
+    enumTranslations: Record<TEnum, string> | DropdownColumnFilterOption[],
     columndDef?: Partial<ColumnDef<TData, TEnum>>,
   ) => ColumnDef<TData, TEnum>;
 }
@@ -18,14 +19,19 @@ const createReactDataTableColumnHelper = <TData extends RowData>(): ReactDataTab
     createEnumColumn: (columnKey, enumTranslations, columndDef) =>
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       columnHelper.accessor(columnKey as any, {
-        cell: (cell) => enumTranslations[cell.getValue()],
+        cell: (cell) =>
+          Array.isArray(enumTranslations)
+            ? enumTranslations.find((x) => x.value === cell.getValue())?.label
+            : enumTranslations[cell.getValue()],
         enableColumnFilter: true,
         meta: {
           dropdownFilter: {
-            options: Object.entries(enumTranslations).map(([key, value]) => ({
-              label: value as string,
-              value: key,
-            })),
+            options: Array.isArray(enumTranslations)
+              ? enumTranslations
+              : Object.entries(enumTranslations).map(([key, value]) => ({
+                  label: value as string,
+                  value: key,
+                })),
           },
         },
         ...columndDef,
