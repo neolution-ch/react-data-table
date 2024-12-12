0ae4effe0d542cfbba0012ba7819ebfd445ef7bb
Sandro Küng <sk@neolution.ch>
2024-12-12 15:54:43 +0100

    added changelog, removed comments

--- src/lib/utils/getCommonPinningStyles.ts
+++ src/lib/utils/getCommonPinningStyles.ts
@@ -1,16 +1,11 @@
 import { Column } from "@tanstack/react-table";
 import { CSSProperties } from "react";
 
-//These are the important styles to make sticky column pinning work!
-//Apply styles like this using your CSS strategy of choice with this kind of logic to head cells, data cells, footer cells, etc.
-//View the index.css file for more needed styles such as border-collapse: separate
+// These are the important styles to make sticky column pinning work!
 const getCommonPinningStyles = <TData>(column: Column<TData>): CSSProperties => {
   const isPinned = column.getIsPinned();
-  //const isLastLeftPinnedColumn = isPinned === "left" && column.getIsLastColumn("left");
-  //const isFirstRightPinnedColumn = isPinned === "right" && column.getIsFirstColumn("right");
 
   return {
-    //boxShadow: isLastLeftPinnedColumn ? "-4px 0 4px -4px gray inset" : isFirstRightPinnedColumn ? "4px 0 4px -4px gray inset" : undefined,
     left: isPinned === "left" ? `${column.getStart("left")}px` : undefined,
     right: isPinned === "right" ? `${column.getAfter("right")}px` : undefined,
     opacity: isPinned ? 1 : 1,
